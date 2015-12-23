5a3ac7bf454624bd2d25b2dd44514afa5a2dcc74
alexd <alexd73@gmail.com>
2015-12-23 09:31:20 +0300

    init

---
+++ markup/modules/head/data/data.js
@@ -0,0 +1,12 @@
+head: {
+    defaults: {
+        useSocialMetaTags: true,
+        title: 'BrandWiFi - маркетинг и продажи в местах с публичной wifi сетью.'
+    },
+    admin: {
+        analytics: {
+            title: 'Аналитика',
+            useSocialMetaTags: false
+        }
+    }
+}
